1462646a5b51b31cf6cea72c854fb77fe0d9d166
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-09-10 17:03:01 +0900

    seedファイルにデータを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+RamenShop.create(name: "天下二品")
+RamenShop.create(name: "二風堂")
+RamenShop.create(name: "武蔵野屋")
+RamenShop.create(name: "ラーメン三郎")
